342b9c2c241951cbf163d3c2f7b39190cc98b810
quangkhuat1995 <quangkhuat1995@gmail.com>
2021-07-19 19:24:12 +0700

    add Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		<BlogPost :post="welcomeScreen" />
+		<BlogPost v-for="(post, index) in samplesBlogPosts" :key="index"/>
+	</div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue'
 export default {
-  name: "Home",
-  components: {},
+	name: "Home",
+	components: {
+		BlogPost,
+	},
+	data() {
+		return {
+			welcomeScreen: {
+				title: "Welcome!",
+				blogPost:
+					"Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+				welcomeScreen: true,
+				photo: "coding",
+			},
+			samplesBlogPosts : [
+				{
+					title: "This is a Filter Title",
+					blogHTML: "This is a filter blog post title!",
+					blogCoverPhoto: "beatiful-stories",
+				},
+				{
+					title: "This is a Filter Title",
+					blogHTML: "This is a filter blog post title!",
+					blogCoverPhoto: "beatiful-stories",
+				}
+			],
+			samplesBlogCards: [
+				{ blogTitle: "Blog Card Title#1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021" },
+				{ blogTitle: "Blog Card Title#2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021" },
+				{ blogTitle: "Blog Card Title#3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021" },
+				{ blogTitle: "Blog Card Title#4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021" },
+			]
+		}
+	}
 };
 </script>
