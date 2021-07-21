cd6bee37e5c06d3c0279265ee5a2bc08fa1d02f0
quangkhuat1995 <quangkhuat1995@gmail.com>
2021-07-21 23:06:40 +0700

    add profile view

--- src/store/index.js
+++ src/store/index.js
@@ -42,12 +42,30 @@ export default new Vuex.Store({
 			state.profileInitials =
 				state.profileFirstName.match(/(\b\S)?/g).join('') + state.profileLastName.match(/(\b\S)?/g).join('');
 		},
+		changeFirstName(state, payload) {
+			state.profileFirstName = payload;
+		},
+		changeLastName(state, payload) {
+			state.profileLastName = payload;
+		},
+		changeUsername(state, payload) {
+			state.profileUsername = payload;
+		},
 	},
 	actions: {
 		async getCurrentUser({ commit }) {
 			const dataBase = await db.collection('users').doc(firebase.auth().currentUser.uid);
 			const dbResults = await dataBase.get();
 			commit('setProfileInfo', dbResults);
+			commit('setProfileInitials');
+		},
+		async updateUserSettings({ commit, state }) {
+			const dataBase = await db.collection('users').doc(state.profileId);
+			await dataBase.update({
+				firstName: state.profileFirstName,
+				lastName: state.profileLastName,
+				username: state.profileUsername,
+			});
 			commit('setProfileInitials');
 		},
 	},
